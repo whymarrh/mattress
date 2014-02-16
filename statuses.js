64df5b35a9945170c295bad8b4a3b30d8836993d
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2014-02-15 20:43:48 -0330

    Add HTTP status codes

---
+++ statuses.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = {
+	"errors": {
+		"BAD_REQUEST": {
+			"statusCode": 400,
+			"message": "Bad request"
+		},
+		"UNAUTHORIZED": {
+			"statusCode": 401,
+			"message": "Unauthorized"
+		},
+		"NOT_FOUND": {
+			"statusCode": 404,
+			"message": "I do not know what you are looking for"
+		},
+		"METHOD_NOT_ALLOWED": {
+			"statusCode": 405,
+			"message": "Method not allowed"
+		}
+	}
+};
